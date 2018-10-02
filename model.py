2f5c49c3a215d0087fbb5ca6deed0d7a24a61ea2
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-02 12:49:05 -0700

    Established foreign key relationships and references

--- model.py
+++ model.py
@@ -41,16 +41,45 @@ class Movie(db.Model):
     released_at = db.Column(db.DateTime, nullable=False)
     imdb_url = db.Column(db.String(200), nullable=False)
 
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"""<title={self.title} 
+                   movie_id={self.movie_id}>"""
+
 
 class Rating(db.Model):
     """Movie Ratings"""
 
     __tablename__ = "ratings"
 
-    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
+    rating_id = db.Column(db.Integer, 
+                          autoincrement=True, 
+                          primary_key=True)
+    movie_id = db.Column(db.Integer,
+                         db.ForeignKey('movies.movie_id'),
+                         nullable=False)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.user_id'), 
+                        nullable=False)
     score = db.Column(db.Integer, nullable=False)
+
+    #Define relationships
+    user = db.relationship("User", 
+                           backref=db.backref("ratings", 
+                                              order_by=rating_id))
+
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings",
+                                               order_by=rating_id))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"""<Rating rating_id={self.rating_id} 
+                   movie_id={self.movie_id} 
+                   user_id={self.user_id} 
+                   score={self.score}>"""    
 
 
 
